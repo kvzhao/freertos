05e97c255b2dd18dbd8f046e0fd0901e29932e6f
kvzhao <kevinzhaio@gmail.com>
2013-10-18 15:07:22 +0800

    UnitTest: test printf() and  itoa, htoa

--- unit_test.c
+++ unit_test.c
@@ -1,7 +1,24 @@
 #include "fio.h"
+#include "string.h"
+#include "util.h"
 #include "unit_test.h"
+
 void unit_test_task(void *pvParameters)
 {
-    char *msg = "\n\rStart Testing\n";
-    fio_write(1, msg, sizeof(msg));
+    char msg1[] = "Start Unit Testing\n\r";
+    char msg2[128] = "Start String testing\n\r";
+    /* test file system */
+    fio_write(1, msg1, strlen(msg1));
+    /* test puts() */
+    puts(msg2);
+    /* test strcmp */
+    if ( strcmp(msg1,msg2)) {
+        puts("[Ok] msg1 ~= msg2\n\r");
+    } else {
+        puts("strcpy result is not match\n\r");
+    }
+    /* test printf */
+    printf("test htoi(255):%s\n\r", htoa(255));
+    printf("test atoi(100):%s\n\r", itoa(100));
+    printf("\n");
 }
